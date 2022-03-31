4db4a0d82528367484a7bef71d1ada7122f94e87
James da Silva <james.dasilva1111@gmail.com>
2022-03-31 14:43:55 +1100

    Added helpful pregame text

--- Assets/Scripts/GameState.cs
+++ Assets/Scripts/GameState.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using UnityEngine;
 using Mirror;
+using TMPro;
 
 /*
 	Documentation: https://mirror-networking.gitbook.io/docs/guides/networkbehaviour
@@ -14,11 +15,6 @@ public class GameState : NetworkBehaviour
 {
     public static GameState Instance;
 
-    void Awake()
-    {
-        Instance = this;
-    }
-
     public int StageCount { get { return m_Stages.Length; } }
     public int CurrentStage => m_CurrentStage;
     public Transform CameraStartPos => m_CameraStartPos;
@@ -26,6 +22,7 @@ public class GameState : NetworkBehaviour
     [SerializeField] Stage[] m_Stages;
     [SerializeField] Ball m_BallPrefab;
     [SerializeField] Transform m_CameraStartPos;
+    [SerializeField] TextMeshProUGUI m_PregameText;
 
     [SyncVar] int m_CurrentStage = -1;
 
@@ -37,6 +34,11 @@ public class GameState : NetworkBehaviour
     public event Action<Player> OnPlayerRemoved;
     public event Action<Player, int> OnStroke;
     public event Action<Player, string> OnPlayerNameSet;
+
+    void Awake()
+    {
+        Instance = this;
+    }
 
     public void AddPlayer(Player a_Player)
     {
@@ -56,6 +58,8 @@ public class GameState : NetworkBehaviour
 
     public void NextStage()
     {
+        m_PregameText.gameObject.SetActive(false);
+
         if (m_CurrentStage == m_Stages.Length - 1)
         {
             // TODO: End Game
@@ -127,57 +131,15 @@ public class GameState : NetworkBehaviour
         OnPlayerNameSet?.Invoke(a_Player, a_Name);
     }
 
-    #region Start & Stop Callbacks
-
-    /// <summary>
-    /// This is invoked for NetworkBehaviour objects when they become active on the server.
-    /// <para>This could be triggered by NetworkServer.Listen() for objects in the scene, or by NetworkServer.Spawn() for objects that are dynamically created.</para>
-    /// <para>This will be called for objects on a "host" as well as for object on a dedicated server.</para>
-    /// </summary>
-    public override void OnStartServer() { }
-
-    /// <summary>
-    /// Invoked on the server when the object is unspawned
-    /// <para>Useful for saving object data in persistent storage</para>
-    /// </summary>
-    public override void OnStopServer() { }
-
-    /// <summary>
-    /// Called on every NetworkBehaviour when it is activated on a client.
-    /// <para>Objects on the host have this function called, as there is a local client on the host. The values of SyncVars on object are guaranteed to be initialized correctly with the latest state from the server when this function is called on the client.</para>
-    /// </summary>
-    public override void OnStartClient() { }
-
-    /// <summary>
-    /// This is invoked on clients when the server has caused this object to be destroyed.
-    /// <para>This can be used as a hook to invoke effects or do client specific cleanup.</para>
-    /// </summary>
-    public override void OnStopClient() { }
-
-    /// <summary>
-    /// Called when the local player object has been set up.
-    /// <para>This happens after OnStartClient(), as it is triggered by an ownership message from the server. This is an appropriate place to activate components or functionality that should only be active for the local player, such as cameras and input.</para>
-    /// </summary>
-    public override void OnStartLocalPlayer() { }
-
-    /// <summary>
-    /// Called when the local player object is being stopped.
-    /// <para>This happens before OnStopClient(), as it may be triggered by an ownership message from the server, or because the player object is being destroyed. This is an appropriate place to deactivate components or functionality that should only be active for the local player, such as cameras and input.</para>
-    /// </summary>
-    public override void OnStopLocalPlayer() {}
-
-    /// <summary>
-    /// This is invoked on behaviours that have authority, based on context and <see cref="NetworkIdentity.hasAuthority">NetworkIdentity.hasAuthority</see>.
-    /// <para>This is called after <see cref="OnStartServer">OnStartServer</see> and before <see cref="OnStartClient">OnStartClient.</see></para>
-    /// <para>When <see cref="NetworkIdentity.AssignClientAuthority">AssignClientAuthority</see> is called on the server, this will be called on the client that owns the object. When an object is spawned with <see cref="NetworkServer.Spawn">NetworkServer.Spawn</see> with a NetworkConnectionToClient parameter included, this will be called on the client that owns the object.</para>
-    /// </summary>
-    public override void OnStartAuthority() { }
-
-    /// <summary>
-    /// This is invoked on behaviours when authority is removed.
-    /// <para>When NetworkIdentity.RemoveClientAuthority is called on the server, this will be called on the client that owns the object.</para>
-    /// </summary>
-    public override void OnStopAuthority() { }
-
-    #endregion
+    public override void OnStartClient()
+    {
+        if (isServer)
+        {
+            m_PregameText.text = "Press Space to Start";
+        }
+        else
+        {
+            m_PregameText.text = "Waiting for Host";
+        }
+    }
 }
