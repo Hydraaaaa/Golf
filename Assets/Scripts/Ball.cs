81871a6027a9c12105742eca20d0bfc22c5c2b39
James da Silva <james.dasilva1111@gmail.com>
2022-04-02 13:14:56 +1100

    Modified ball physics to make balls reach a complete stop sooner
    Not a final version of physics, but good enough to facilitate proper ball swing logic

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -14,10 +14,19 @@ public class Ball : NetworkBehaviour
     public Rigidbody Rigidbody => m_Rigidbody;
 
     [SerializeField] Rigidbody m_Rigidbody;
+    [SerializeField] float m_Drag = 0.01f;
     public Player Player { get; set; }
 
     public override void OnStartLocalPlayer()
     {
         Debug.Log("Ball.OnStartAuthority");
+    }
+
+    void Update()
+    {
+        if (hasAuthority)
+        {
+            m_Rigidbody.velocity = m_Rigidbody.velocity.normalized * Mathf.Clamp(m_Rigidbody.velocity.magnitude - Time.deltaTime * m_Drag, 0, float.MaxValue);
+        }
     }
 }
